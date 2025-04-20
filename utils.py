03de99c6b6cdc6ad4b20c077430528b665c66d9f
Aditya Mitra <adityaarghya0@gmail.com>
2025-04-20 11:20:15 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -22,7 +22,7 @@ def flag_bytes(flag):
 def check_valid_conn(nac, ip, port):
 	return True #Modify this function to blacklist machines
 	
-def segment_payload(payload, maxsize=100):
+def segment_payload(payload, maxsize=1400):
 	total_packets=math.ceil(len(payload)/maxsize)
 	fragments=[]
 	for i in range(total_packets):
