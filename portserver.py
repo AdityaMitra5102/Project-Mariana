09391fb769581d016d8cc736a4ae20a653b67086
Aditya Mitra <adityaarghya0@gmail.com>
2025-04-26 15:47:26 +0530

    Update portserver.py

--- portserver.py
+++ portserver.py
@@ -8,7 +8,7 @@ def process_port_payload_from_tunnel(nac, payload, send_payload, securityconfig)
 	mode, servermode, sourceport, destport, data=process_payload(payload)
 	try:
 		destportint= int.from_bytes(destport)
-		if not (desportint in securityconfig['port_fw_allow'] or '*' in securityconfig['port_fw_allow']):
+		if not (destportint in securityconfig['port_fw_allow'] or '*' in securityconfig['port_fw_allow']):
 			logging.info('Port blocked. Packet dropped.')
 			return
 	except:
