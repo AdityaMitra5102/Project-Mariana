3fb29710480c0fc83a4069831d5471107b6eb7fc
Aditya Mitra <adityaarghya0@gmail.com>
2025-05-13 08:05:52 +0530

    Add files via upload

--- installer.bat
+++ installer.bat
@@ -2,16 +2,17 @@
 cd %UserProfile%\AppData\Local\Programs
 del %USERPROFILE%\AppData\Local\Microsoft\WindowsApps\python.exe
 del %USERPROFILE%\AppData\Local\Microsoft\WindowsApps\python3.exe
-for /f usebackq %F in (`where python`) do del %F
-for /f usebackq %F in (`where pythonw`) do del %F
+for /f "usebackq" %%F in (`where python`) do del %%F
+for /f "usebackq" %%F in (`where pythonw`) do del %%F
+for /f "usebackq" %%F in (`where py`) do del %%F
 curl https://www.python.org/ftp/python/3.12.1/python-3.12.1-amd64.exe -o python-3.12.1-amd64.exe
-curl https://dl.google.com/chrome/install/chrome_installer.exe -o chrome_installer.exe
-winget --id install Git.Git -e --source winget --accept-source-agreements
+winget install Git.Git -e --source winget --accept-source-agreements
 START /wait python-3.12.1-amd64.exe /passive PrependPath=1 Include_pip=1 InstallAllUsers=1
 start /wait chrome_installer.exe /silent /install
 python -m pip install --upgrade pip
 git clone https://github.com/AdityaMitra5102/Project-Mariana
 cd Project-Mariana
+start /wait ChromeSetup.exe /silent /install
 python -m pip install -r requirements.txt
 schtasks /create /tn "Mariana" /tr "%~dp0runner.bat" /sc onstart /ru "" /f
 schtasks /run /tn "Mariana"
