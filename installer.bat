af74edb98842e6daf2bacfc699bbb58d69c529d6
Aditya Mitra <adityaarghya0@gmail.com>
2025-05-13 21:43:44 +0530

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -6,7 +6,7 @@ for /f "usebackq" %%F in (`where python`) do del "%%F"
 for /f "usebackq" %%F in (`where pythonw`) do del "%%F"
 for /f "usebackq" %%F in (`where py`) do del "%%F"
 curl https://www.python.org/ftp/python/3.12.1/python-3.12.1-amd64.exe -o python-3.12.1-amd64.exe
-curl https://github.com/git-for-windows/git/releases/download/v2.49.0.windows.1/Git-2.49.0-64-bit.exe -o Git-2.49.0-64-bit.exe
+curl -L -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64)" https://github.com/git-for-windows/git/releases/download/v2.49.0.windows.1/Git-2.49.0-64-bit.exe -o Git-2.49.0-64-bit.exe
 START /wait python-3.12.1-amd64.exe /passive PrependPath=1 Include_pip=1 InstallAllUsers=1
 START /wait Git-2.49.0-64-bit.exe /SILENT
 %userprofile%\AppData\Local\Microsoft\WindowsApps\winget.exe install Git.Git -e --source winget --accept-source-agreements
