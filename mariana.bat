1560fce9e87f96fe9b98f0ce960c31a643023b82
Aditya Mitra <adityaarghya0@gmail.com>
2025-05-13 22:17:44 +0530

    Update mariana.bat

--- mariana.bat
+++ mariana.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Google\Chrome\Application\chrome.exe" --incognito --proxy-server=http://localhost:8000 http://local.mariana
+start /B "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --incognito --proxy-server=http://localhost:8000 http://local.mariana
