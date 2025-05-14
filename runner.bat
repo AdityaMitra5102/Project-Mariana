76190a295112ad64d61a13697de61c515d6589ae
Aditya Mitra <adityaarghya0@gmail.com>
2025-05-14 16:19:44 +0530

    Update runner.bat

--- runner.bat
+++ runner.bat
@@ -1,6 +1,8 @@
 @ECHO off
 ECHO Checking for Mariana Updates
 cd %~dp0
+taskkill /F /IM python.exe
+taskkill /F /IM pythow.exe
 git config --global --add safe.directory %~dp0
 git pull
 python -m pip install --upgrade pip
