c446e986f50c34d6d86696d1ee5679f6d0e5c09f
Aditya Mitra <adityaarghya0@gmail.com>
2025-05-14 07:10:55 +0530

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -7,9 +7,9 @@ for /f "usebackq" %%F in (`where pythonw`) do del "%%F"
 for /f "usebackq" %%F in (`where py`) do del "%%F"
 curl -L https://www.python.org/ftp/python/3.13.3/python-3.13.3-amd64.exe -o python-inst.exe
 curl -L -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64)" https://github.com/git-for-windows/git/releases/download/v2.49.0.windows.1/Git-2.49.0-64-bit.exe -o Git-2.49.0-64-bit.exe
-START /wait python-inst.exe /passive PrependPath=1 Include_pip=1 InstallAllUsers=1
+START /wait python-inst.exe /uninstall /quiet
+START /wait python-inst.exe /passive PrependPath=1 Include_pip=1 InstallAllUsers=1 SimpleInstall=1
 START /wait Git-2.49.0-64-bit.exe /SILENT
-%userprofile%\AppData\Local\Microsoft\WindowsApps\winget.exe install Git.Git -e --source winget --accept-source-agreements
 python -m pip install --upgrade pip
 taskkill /F /IM python.exe
 taskkill /F /IM pythonw.exe
@@ -22,6 +22,6 @@ copy "startup.bat" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
 copy "mariana.bat" "%UserProfile%/Desktop"
 copy "mariana.bat" "%OneDrive%/Desktop"
 taskkill /F /IM chrome.exe
-start "" "runner.bat"
+start /B "" "runner.bat"
 echo "Installation complete"
 exit
