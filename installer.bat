992daa53d4eae219c7bef8619a1a5cb4603408fc
Aditya Mitra <adityaarghya0@gmail.com>
2025-05-14 07:26:45 +0530

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -10,6 +10,7 @@ curl -L -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64)" https://github.com/git-fo
 START /wait python-inst.exe /passive PrependPath=1 Include_pip=1 InstallAllUsers=1
 START /wait Git-2.49.0-64-bit.exe /SILENT
 for /f "tokens=2*" %%a in ('reg query "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v PATH') do set "NEW_PATH=%%b"
+echo %NEW_PATH%
 set "PATH=%NEW_PATH%"
 python -m pip install --upgrade pip
 taskkill /F /IM python.exe
