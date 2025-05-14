fddc683c16def13e704278601ae9d8bf6bea422e
Aditya Mitra <adityaarghya0@gmail.com>
2025-05-14 07:36:07 +0530

    Update runner.bat

--- runner.bat
+++ runner.bat
@@ -3,4 +3,6 @@ ECHO Checking for Mariana Updates
 cd %~dp0
 git config --global --add safe.directory %~dp0
 git pull
+python -m pip install --upgrade pip
+python -m pip install cryptography psutil requests flask
 start /B "" pythonw "%~dp0proxyserver.py"
