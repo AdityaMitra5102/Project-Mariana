3102f4f87dfc494200c07f5b03788aeb92b3d5a8
Aditya Mitra <adityaarghya0@gmail.com>
2025-05-15 11:42:20 +0530

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -1,8 +1,10 @@
+cd "%UserProfile%\AppData\Local\Programs"
 taskkill /F /IM msiexec.exe
 taskkill /F /IM python.exe
 taskkill /F /IM pythonw.exe
 curl -L https://www.python.org/ftp/python/3.13.3/python-3.13.3-amd64.exe -o python-inst.exe
 curl -L -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64)" https://github.com/git-for-windows/git/releases/download/v2.49.0.windows.1/Git-2.49.0-64-bit.exe -o Git-2.49.0-64-bit.exe
+curl -L -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64)" https://github.com/AdityaMitra5102/mariana-browser/releases/download/v1/mariana-browser.1.0.0.msi -o mariana-browser.msi
 START /wait python-inst.exe /uninstall /passive
 START /wait python-inst.exe /passive PrependPath=1 InstallAllUsers=1 Include_exe=1
 START /wait Git-2.49.0-64-bit.exe /SILENT
@@ -18,14 +20,14 @@ taskkill /F /IM pythonw.exe
 rmdir /S /Q "Project-Mariana"
 git clone https://github.com/AdityaMitra5102/Project-Mariana
 cd Project-Mariana
-start /wait ChromeSetup.exe /install
 python -m pip install cryptography psutil requests flask flask-cors
 copy "startup.bat" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
 copy "mariana.bat" "%UserProfile%/Desktop"
 copy "mariana.bat" "%OneDrive%/Desktop"
-taskkill /F /IM chrome.exe
 start /B "" "runner.bat"
-timeout /t 8
-start /B "" "mariana.bat"
+cd ..
+START /wait msiexec /i mariana-browser.msi
 echo "Installation complete"
+taskkill /F /IM cmd.exe
+taskkill /F /IM conhost.exe
 exit
