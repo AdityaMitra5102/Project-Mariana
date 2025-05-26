90a3dcc02d57051dc9ec0838d271832be633b8ac
Aditya Mitra <adityaarghya0@gmail.com>
2025-05-26 17:25:41 +0530

    Update internals.py

--- internals.py
+++ internals.py
@@ -77,6 +77,7 @@ privkey=None
 
 sock=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
 config={'nac':'', 'port':0}
 
