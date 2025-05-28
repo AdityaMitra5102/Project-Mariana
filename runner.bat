ed69e7b7264435d32f777ac68de48b08e35821c8
Aditya Mitra <adityaarghya0@gmail.com>
2025-05-28 06:59:20 +0530

    Update runner.bat

--- runner.bat
+++ runner.bat
@@ -8,8 +8,9 @@ start "" wscript "%temp%\msg.vbs"
 
 taskkill /F /IM python.exe
 taskkill /F /IM pythow.exe
+
+start /B "" pythonw "%~dp0wintray.py"
 git config --global --add safe.directory %~dp0
 git pull
 python -m pip install --upgrade pip
 python -m pip install cryptography psutil requests flask flask-cors
-start /B "" pythonw "%~dp0wintray.py"
