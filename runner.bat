5b1c0c4ca13f4761a22c3c469b14da6bd09fdb1d
Aditya Mitra <adityaarghya0@gmail.com>
2025-06-04 06:11:43 +0530

    Update runner.bat

--- runner.bat
+++ runner.bat
@@ -4,6 +4,7 @@ cd %~dp0
 
 echo MsgBox "Starting Mariana's Qubit!",64,"Please wait" > %temp%\msg.vbs
 start "" wscript "%temp%\msg.vbs"
+python -m pip install cryptography psutil requests flask flask-cors pystray pillow
 git config --global --add safe.directory %~dp0
 git stash
 git pull
