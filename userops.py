7b5dced7486e359f3bb193d86fd10c52d3b19afb
Aditya Mitra <adityaarghya0@gmail.com>
2025-09-18 22:26:54 +0530

    Add files via upload

--- userops.py
+++ userops.py
@@ -1,7 +1,7 @@
-
+import proxyutils
 def user_response(source_nac, payload, send_payload, phone_book_reverse_lookup, securityconfig):
-	print(f'{source_nac}>>> {payload.decode()}')
-	return None #No reply
+	#print(f'{source_nac}>>> {payload.decode()}')
+	return proxyutils.user_response(source_nac, payload, send_payload, phone_book_reverse_lookup, securityconfig)
 	
 	
 def user_input():
